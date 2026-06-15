a332bb6d6c6df17d4c916a0a005cb136a7e94c60
voyager003 <miraclex000@gmail.com>
2026-06-15 14:35:08 +0900

    Initial commit

---
+++ src/main/java/com/eventlogpipeline/EventLogPipelineApplication.java
@@ -0,0 +1,13 @@
+package com.eventlogpipeline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventLogPipelineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EventLogPipelineApplication.class, args);
+    }
+
+}
